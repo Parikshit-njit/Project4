ad1b1a7335b4ab5a96e3c3b3a0d7700eb38752de
Parikshit Narang <pn24@njit.edu>
2021-07-26 00:49:37 -0400

    Task: created model for addressesData

---
+++ app/models.py
@@ -0,0 +1,50 @@
+from . import db
+
+
+class Addresses(db.Model):
+    """Data model for user addresses."""
+
+    __tablename__ = 'addresses'
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+    fname = db.Column(
+        db.String(64),
+        index=False,
+        unique=True,
+        nullable=True
+    )
+    lname = db.Column(
+        db.String(80),
+        index=False,
+        unique=True,
+        nullable=True
+    )
+    address = db.Column(
+        db.String(80),
+        index=False,
+        unique=True,
+        nullable=True
+    )
+    city = db.Column(
+        db.String(80),
+        index=False,
+        unique=True,
+        nullable=True
+    )
+    state = db.Column(
+        db.String(80),
+        index=False,
+        unique=True,
+        nullable=True
+    )
+    zip_code = db.Column(
+        db.String(80),
+        index=False,
+        unique=True,
+        nullable=True
+    )
+
+    def __repr__(self):
+        return '<Addresses {}>'.format(self.id)
