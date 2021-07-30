8de389dbc1421a1e0d8d177dcf37ef574b59906a
Parikshit Narang <pn24@njit.edu>
2021-07-30 10:24:03 -0400

    Task: added models in modelspy

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,6 @@
-from collections import OrderedDict
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from app import db
+
 
 class AddressForm(FlaskForm):
     fname = StringField("First Name")
@@ -12,60 +11,3 @@ class AddressForm(FlaskForm):
     zip_code = StringField("Zip Code")
     submit = SubmitField("Submit")
 
-class Addresses(db.Model):
-    """Data model for user addresses."""
-    __table_args__ = (
-        db.UniqueConstraint('fname', 'lname', 'address', 'state', 'city', 'zip_code'),
-    )
-    __tablename__ = 'addresses'
-    id = db.Column(
-        db.Integer,
-        primary_key=True
-    )
-
-    fname = db.Column(
-        db.String(64),
-        index=False,
-        unique=False,
-        nullable=True
-    )
-
-    lname = db.Column(
-        db.String(80),
-        index=False,
-        unique=False,
-        nullable=True
-    )
-    address = db.Column(
-        db.String(80),
-        index=False,
-        unique=False,
-        nullable=True
-    )
-    city = db.Column(
-        db.String(80),
-        index=False,
-        unique=False,
-        nullable=True
-    )
-    state = db.Column(
-        db.String(80),
-        index=False,
-        unique=False,
-        nullable=True
-    )
-    zip_code = db.Column(
-        db.String(80),
-        index=False,
-        unique=False,
-        nullable=True
-    )
-
-    def toDict(self):
-        result = OrderedDict()
-        for key in self.__mapper__.c.keys():
-            result[key] = str(getattr(self, key))
-        return result
-
-    def __repr__(self):
-        return '<Addresses {}>'.format(self.id)
--- app/models.py
+++ app/models.py
@@ -1,55 +1,61 @@
-# from flask_sqlalchemy import SQLAlchemy
-# from app import app
-#
-# db = SQLAlchemy(app)
-#
-#
-# class Addresses(db.Model):
-#     """Data model for user addresses."""
-#
-#     __tablename__ = 'addresses'
-#     id = db.Column(
-#         db.Integer,
-#         primary_key=True
-#     )
-#
-#     fname = db.Column(
-#         db.String(64),
-#         index=False,
-#         unique=True,
-#         nullable=True
-#     )
-#
-#     lname = db.Column(
-#         db.String(80),
-#         index=False,
-#         unique=True,
-#         nullable=True
-#     )
-#     address = db.Column(
-#         db.String(80),
-#         index=False,
-#         unique=True,
-#         nullable=True
-#     )
-#     city = db.Column(
-#         db.String(80),
-#         index=False,
-#         unique=True,
-#         nullable=True
-#     )
-#     state = db.Column(
-#         db.String(80),
-#         index=False,
-#         unique=True,
-#         nullable=True
-#     )
-#     zip_code = db.Column(
-#         db.String(80),
-#         index=False,
-#         unique=True,
-#         nullable=True
-#     )
-#
-#     def __repr__(self):
-#         return '<Addresses {}>'.format(self.id)
+from collections import OrderedDict
+from app import db
+
+
+class Addresses(db.Model):
+    """Data model for user addresses."""
+    __table_args__ = (
+        db.UniqueConstraint('fname', 'lname', 'address', 'state', 'city', 'zip_code'),
+    )
+    __tablename__ = 'addresses'
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    fname = db.Column(
+        db.String(64),
+        index=False,
+        unique=False,
+        nullable=True
+    )
+
+    lname = db.Column(
+        db.String(80),
+        index=False,
+        unique=False,
+        nullable=True
+    )
+    address = db.Column(
+        db.String(80),
+        index=False,
+        unique=False,
+        nullable=True
+    )
+    city = db.Column(
+        db.String(80),
+        index=False,
+        unique=False,
+        nullable=True
+    )
+    state = db.Column(
+        db.String(80),
+        index=False,
+        unique=False,
+        nullable=True
+    )
+    zip_code = db.Column(
+        db.String(80),
+        index=False,
+        unique=False,
+        nullable=True
+    )
+
+    def toDict(self):
+        result = OrderedDict()
+        for key in self.__mapper__.c.keys():
+            result[key] = str(getattr(self, key))
+        return result
+
+    def __repr__(self):
+        return '<Addresses {}>'.format(self.id)
